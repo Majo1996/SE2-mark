512fbe5a25dd09753871277bbf61de8e741cdfe3
Majo1996 <ma.jo1996@live.nl>
2014-12-12 11:11:04 +0100

    first commit

---
+++ SE2 ontwikkelopdracht/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SE2_ontwikkelopdracht.Global" Language="C#" %>
---
+++ SE2 ontwikkelopdracht/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SE2_ontwikkelopdracht.Startup))]
+namespace SE2_ontwikkelopdracht
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
